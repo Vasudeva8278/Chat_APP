915bafa239ca494c551819614954c3363dec78cc
vasudev8278 <vasudevsanchapu@gmail.com>
2024-09-01 16:36:58 +0530

    commit

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('chat message', (msg) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit('chat message', message);
+    setMessage('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <h1>Real-Time Chat</h1>
+      <div>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message..."
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
